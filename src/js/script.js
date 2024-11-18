6ceeb7df915d02b4fe68773040e2a94e33f80fdb
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-11-18 18:00:42 +0500

    implement webpack config

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,18 @@
-// Импортировать CSS Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-// Импортировать JS Bootstrap
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-// Импортировать icons Bootstrap
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../css/style.css';
+
+console.log('2');
+document.addEventListener('DOMContentLoaded', () => {
+  const burgerMenu = document.querySelector('.burger-menu');
+  const mobileNav = document.querySelector('.mobile-nav');
+
+  if (burgerMenu && mobileNav) {
+    burgerMenu.addEventListener('click', () => {
+      mobileNav.classList.toggle('active');
+    });
+  } else {
+    console.error('Элементы бургер-меню или навигации не найдены!');
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,58 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/script.js',
+  entry: './src/js/script.js', // Твой основной JS файл
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'), 
+    path: path.resolve(__dirname, 'dist'), // Папка для сборки
+    filename: 'js/[name].[contenthash].js', // Выходной файл JS с хешем для кеширования
+    clean: true, // Очистка папки dist перед новой сборкой
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.js$/, // Применяем для всех JS файлов
+        exclude: /node_modules/,
+        use: 'babel-loader', // Для транспиляции JS через Babel (если нужно)
       },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
+        test: /\.css$/, // Применяем для всех CSS файлов
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(ico)$/i, // Для иконок
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/icons/[name].[contenthash][ext]', // Иконки в папку dist/assets/icons
+        },
       },
     ],
   },
-  resolve: {
-    alias: {
-      bootstrap: path.resolve(__dirname, 'node_modules/bootstrap'),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Указываем исходный HTML
+      filename: 'index.html', // Выходной файл HTML
+      inject: 'body', // Вставка скриптов в body
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css', // Выходной файл CSS с хешем
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'), // Папка public будет обслуживаться сервером
     },
+    port: 8080, // Порт для локального сервера
+    open: true, // Открытие браузера при старте сервера
+    hot: true,  // Горячая перезагрузка
+    compress: true, // Сжатие файлов при раздаче
   },
-  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js'], // Расширения для файлов
+  },
+  mode: 'development', // Режим разработки
 };

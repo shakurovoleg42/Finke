426f68f7ed1136fc4911a844b8c59ee58e619807
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-11-18 12:14:19 +0500

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+// Импортировать CSS Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Импортировать JS Bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+console.log('АААААААААА ПОЛУЧИЛОСЬ')
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/script.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      bootstrap: path.resolve(__dirname, 'node_modules/bootstrap'),
+    },
+  },
+  devtool: 'source-map',
+};

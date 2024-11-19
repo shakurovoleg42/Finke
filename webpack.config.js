7eeec24c8073ee1de8ec924996cd056d8a7edefb
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-11-19 09:09:46 +0500

    webpack config created

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,72 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/script.js', // Твой основной JS файл
+  entry: './src/js/script.js',
   output: {
-    path: path.resolve(__dirname, 'dist'), // Папка для сборки
-    filename: 'js/[name].[contenthash].js', // Выходной файл JS с хешем для кеширования
-    clean: true, // Очистка папки dist перед новой сборкой
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[contenthash].js',
+    clean: true,
   },
   module: {
     rules: [
       {
-        test: /\.js$/, // Применяем для всех JS файлов
+        test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader', // Для транспиляции JS через Babel (если нужно)
+        use: 'babel-loader',
       },
       {
-        test: /\.css$/, // Применяем для всех CSS файлов
+        test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
         ],
       },
       {
-        test: /\.(ico)$/i, // Для иконок
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'assets/icons/[name].[contenthash][ext]', // Иконки в папку dist/assets/icons
+          filename: 'assets/images/[name].[contenthash][ext]',
+        },
+      },
+      {
+        test: /\.(ico)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/icons/[name].[contenthash][ext]',
         },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html', // Указываем исходный HTML
-      filename: 'index.html', // Выходной файл HTML
-      inject: 'body', // Вставка скриптов в body
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
     }),
     new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash].css', // Выходной файл CSS с хешем
+      filename: 'css/[name].[contenthash].css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '' },
+      ],
     }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'), // Папка public будет обслуживаться сервером
+      directory: path.join(__dirname, 'public'),
     },
-    port: 8080, // Порт для локального сервера
-    open: true, // Открытие браузера при старте сервера
-    hot: true,  // Горячая перезагрузка
-    compress: true, // Сжатие файлов при раздаче
+    port: 8080,
+    open: true,
+    hot: true,
+    compress: true,
   },
   resolve: {
-    extensions: ['.js'], // Расширения для файлов
+    extensions: ['.js'],
   },
-  mode: 'development', // Режим разработки
+  mode: 'development',
 };
+

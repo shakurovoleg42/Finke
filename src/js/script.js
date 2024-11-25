77a976609aa378f632b3a5a8697f3e003f026f42
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-11-25 17:58:00 +0500

    fix js

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../assets/css/style.css';
 
 

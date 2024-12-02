fa1c2379bb328ccb2248768ce8ea25c3f7d753ca
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-12-02 10:25:52 +0500

    fix burger menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM Content Loaded Successfully");
 
+  console.log("dom content loaded successfully")
+  const burgerMenu = document.querySelector('.burger-menu');
+  const mobileNav = document.querySelector('.mobile-nav');
+  const closeBtn = document.querySelector('.close-btn');
+
+  if (burgerMenu && mobileNav && closeBtn) {
+    // Открытие бургер-меню
+    burgerMenu.addEventListener('click', () => {
+      mobileNav.classList.toggle('active');
+      burgerMenu.classList.toggle('open');
+    });
+
+    // Закрытие бургер-меню
+    closeBtn.addEventListener('click', () => {
+      mobileNav.classList.remove('active');
+      burgerMenu.classList.remove('open');
+    });
+  } else {
+    console.error('Элементы бургер-меню или навигации не найдены!');
+  }
+
   const productLinks = {
     "Моторные масла для легковых автомобилей": "http://finke-oil.ru/shop/dlya-legkovyx-avtomobilej/",
     "Моторные масла для коммерческого транспорта": "http://finke-oil.ru/shop/dlya-gruzovyx-avtomobilej/",
